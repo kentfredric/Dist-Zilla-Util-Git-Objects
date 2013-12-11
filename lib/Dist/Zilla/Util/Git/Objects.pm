732b24d336662b6e407f321dbdc588faf40c7ed3
Kent Fredric <kentfredric@gmail.com>
2013-12-11 14:12:08 +1300

    Document guts

--- lib/Dist/Zilla/Util/Git/Objects.pm
+++ lib/Dist/Zilla/Util/Git/Objects.pm
@@ -10,6 +10,26 @@ use Try::Tiny;
 use Scalar::Util qw(blessed);
 use MooseX::LazyRequire;
 
+=head1 SYNOPSIS
+
+    my $o = Dist::Zilla::Util::Git::Objects->new(
+        zilla => $self->zilla
+    );
+    my $b = Dist::Zilla::Util::Git::Branches->new(
+        zilla => $self->zilla
+    );
+    my $commit = $o->get_commit( $b->get_branch('master')->sha1 );
+    print "Last commit message is" . join q[\n] , @{ $commit->object_content };
+
+    my $tree  = $o->get_commit( $commit->get_header_string('tree' ) );
+
+    for my $entry (@{ $tree->entries } ) {
+        printf "%s %s\n", $entry->mode, $entry->filename;
+    }
+
+
+=cut
+
 has git   => ( is => ro =>, isa => Object =>, lazy_build    => 1 );
 has zilla => ( is => ro =>, isa => Object =>, lazy_required => 1 );
 has typemap => ( is => ro =>, isa => HashRef => lazy_build => 1 );
@@ -102,6 +122,23 @@ sub _rev_parse {
 
 }
 
+=method C<get_object>
+
+Resolve the given name to an object
+
+    my $object = $dzugo->get_object('deadbeefc0ffee');
+
+Can also resolve any rev that resolves to a commit
+
+    my $object = $dzugo->get_object('master');
+
+Though this is not recommended due to ambiguity potential.
+
+    my $object = $dzugo->get_object( $b->get_branch('master')->sha1  );
+    my $object = $dzugo->get_object( $b->get_tag('master')->sha1  );
+
+=cut
+
 sub get_object {
   my ( $self, $desc ) = @_;
   my $rev;
@@ -110,6 +147,23 @@ sub get_object {
   my ($type) = $self->git->cat_file( '-t', $rev );
   return $self->_inflate_type( $type, $rev );
 }
+
+=method C<get_commit_at>
+
+Resolve a C<rev> to a commit.
+
+This is only really useful in cases where C<rev> is possibly a resolvant for an annotated C<tag>.
+
+Here, C<get_commit_at> will follow the annotated C<tag> to find the C<commit>.
+
+So you should always use this method instead of calling C<get_object> directly if you're always
+expecting to git a C<commit> back.
+
+    my $o = $dzugo->get_object('1.2.0'); # Resolves to annotated tag
+
+    my $o = $dzugo->get_commit_at('1.2.0'); # Annotated tag resolves to commit
+
+=cut
 
 sub get_commit_at {
   my ( $self, $desc ) = @_;
@@ -125,6 +179,22 @@ sub get_commit_at {
   require Carp;
   Carp::croak( sprintf q[Cannot resolve a commit from %s (type = %s)], $desc, $object->type );
 }
+
+=method C<get_tree_at>
+
+Like C<get_commit_at>, this method will follow object pointers digging until it finds a C<tree> object.
+
+So this means:
+
+    ref → annotated tag → commit → tree →
+    ref →                 commit → tree →
+    ref →                          tree →
+
+This is useful in cases where you expect a tree always.
+
+    my $t = $dzguo->get_tree_at('master'); # masters tree
+
+=cut
 
 sub get_tree_at {
   my ( $self, $desc ) = @_;
@@ -144,6 +214,31 @@ sub get_tree_at {
   require Carp;
   Carp::croak( sprintf q[Cannot resolve a tree from %s (type = %s)], $desc, $object->type );
 }
+
+=method C<get_tree_at_path>
+
+This Further extends upon the logic of C<get_tree_at> and will resolve tree objects
+that are found at the given path at the given commit.
+
+    my $t = $dzugo->get_tree_at('master', 'lib' );
+    my $t = $dzugo->get_tree_at('master', 'lib/Dist' );
+    my $t = $dzugo->get_tree_at('master', 'lib/Dist/Foo' );
+
+Implementationwise, this invokes
+
+    my $sha = $dzugo->get_tree_at('master')->sha1;
+
+In order to dig down to a tree of some description based on the search term.
+
+If the search term is already a tree ref, then it proceeds to do path traversal, returning undef
+as soon as it can't proceed further, returning the tree at the given path.
+
+Though, it will always resolve to a C<tree>
+
+If you specified the path to a C<blob>, this will return C<undef>.
+
+
+=cut
 
 sub get_tree_at_path {
   my ( $self, $desc, $path ) = @_;
@@ -165,6 +260,25 @@ sub get_tree_at_path {
   }
   return;
 }
+
+=method C<get_blob_at>
+
+This method resolves a path to a blob ( which you will probably see as being a files contents ).
+
+    my $blob = $dzugo->get_blob_at('master','.gitignore');
+    my $blob = $dzugo->get_blob_at('master','lib/Foo.pm');
+
+This is implemented in terms of
+
+    my $tree =  $dzugo->get_blob_at('master','lib');
+    my $entry = $tree->entry_named('Foo.pm');
+    return $dzugo->get_object($entry);
+
+But will fail if C<Foo.pm> is a tree instead of a blob.
+
+Otherwise, if the path does not exist at the give commit, it will return undef.
+
+=cut
 
 sub get_blob_at {
   my ( $self, $commit, $path ) = @_;
