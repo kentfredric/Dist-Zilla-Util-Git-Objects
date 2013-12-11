717cf14832cbb2c1a8ae4ae675051af8b56bfd95
Kent Fredric <kentfredric@gmail.com>
2013-12-11 13:06:56 +1300

    Tidy Role::Headers

--- lib/Dist/Zilla/Util/Git/Objects/Role/Headers.pm
+++ lib/Dist/Zilla/Util/Git/Objects/Role/Headers.pm
@@ -1,6 +1,7 @@
 use strict;
 use warnings;
 use utf8;
+
 package Dist::Zilla::Util::Git::Objects::Role::Headers;
 
 # ABSTRACT: A role for object types that have headers
@@ -36,6 +37,7 @@ sub _build_object_headers {
   }
   return $headers;
 }
+
 sub _build_tag_content {
   my ($self)  = @_;
   my (@lines) = ( @{ $self->raw_content } );
@@ -48,9 +50,9 @@ sub _build_tag_content {
 }
 
 sub has_header {
-    my ( $self, $header ) = @_;
-    return unless exists $self->object_headers->{$header};
-    return 1;
+  my ( $self, $header ) = @_;
+  return unless exists $self->object_headers->{$header};
+  return 1;
 }
 
 =method C<get_header>
@@ -84,29 +86,30 @@ Examples:
 =cut
 
 sub get_header {
-    my ( $self, $header ) = @_;
-    if ( not $self->has_header($header) ) {
-        require Carp;
-        Carp::croak(qq[This object does not have the header `$header`] );
+  my ( $self, $header ) = @_;
+  if ( not $self->has_header($header) ) {
+    require Carp;
+    Carp::croak(qq[This object does not have the header `$header`]);
+  }
+  my @out;
+  my $current;
+  for my $line ( @{ $self->object_headers->{$header} } ) {
+    if ( $line =~ /\A $header [ ] (.+) \z/msx ) {
+      push @out, $current if defined $current;
+      $current = ["$1"];
+      next;
     }
-    my @out;
-    my $current;
-    for my $line ( @{ $self->object_headers->{$header} } ) {
-        if ( $line =~ /\A $header [ ] (.+) \z/msx ) {
-            push @out, $current if defined $current;
-            $current = [ "$1" ];
-            next;
-        }
-        if ( $line =~ /\A [ ] (.+) \z/msx ) {
-            push @{$current}, "$1";
-            next;
-        }
-        require Carp;
-        Carp::confess("Parse error in headers, $line did not match a rule");
+    if ( $line =~ /\A [ ] (.+) \z/msx ) {
+      push @{$current}, "$1";
+      next;
     }
-    push @out, $current;
-    return @out;
+    require Carp;
+    Carp::confess("Parse error in headers, $line did not match a rule");
+  }
+  push @out, $current;
+  return @out;
 }
+
 =method C<get_header_strings>
 
 Returns a list of headers with this name as strings
@@ -138,13 +141,13 @@ Examples:
 =cut
 
 sub get_header_strings {
-    my ( $self, $header ) = @_;
-    my ( @headers ) = $self->get_header($header);
-    my @out;
-    for my $header ( @headers ) {
-        push @out, join qq[\n], @{ $header };
-    }
-    return @out;
+  my ( $self, $header ) = @_;
+  my (@headers) = $self->get_header($header);
+  my @out;
+  for my $header (@headers) {
+    push @out, join qq[\n], @{$header};
+  }
+  return @out;
 }
 
 no Moose::Role;
